9a82dc71e0b36a4daa3daa3788da1084902a6b1b
oskarlh <g@oskar-lh.name>
2020-09-26 00:32:19 +0200

    Build

---
+++ precache-manifest.6be29689a0cd102726108460d5781c88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a79eae55148e5eb317eca499b9e968",
+    "url": "/invoices/index.html"
+  },
+  {
+    "revision": "fd0ef1307e65ee148f3c",
+    "url": "/invoices/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "54c23fbe30d05a408812",
+    "url": "/invoices/static/js/2.e7e63e4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/invoices/static/js/2.e7e63e4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd0ef1307e65ee148f3c",
+    "url": "/invoices/static/js/main.05f7d611.chunk.js"
+  },
+  {
+    "revision": "c5cabe499b01b36b01be",
+    "url": "/invoices/static/js/runtime-main.1dd6f923.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/invoices/static/media/logo.5d5d9eef.svg"
+  }
+]);
